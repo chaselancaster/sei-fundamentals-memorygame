17ff7a5111e27189516cb7db4a5a7364aac05475
chaselancaster <chaseblancaster@gmail.com>
2019-03-07 17:47:53 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 // Leaving these two arrays outside of the flipCard function since we want them to have global scope.
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+];
 
 var cardsInPlay = [];
 
@@ -15,12 +36,17 @@ var checkForMath = function() {
 // Function to store all steps that should happen when the user flips a card.
 
 var flipCard = function(cardId) {
-    console.log("User flipped " + cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
     //adding the card user flipped to cardsInPlay array
-    cardsInPlay.push(cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
+    //So we can "see" the cards that are flipped in our console.
+    console.log(cards[cardId].cardImage);
+    console.log('Suit: ' + cards[cardId].suit);
     if (cardsInPlay.length === 2) {
         checkForMath();
     }
+    
+    
 
     //If statement that checks if length of cardsInPlay array is 2
     if (cardsInPlay.length === 2) {
